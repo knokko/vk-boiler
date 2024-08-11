29c7cee2f56476e67cdbd70fe43154c2a3442921
knokko <knokogator@hotmail.com>
2024-08-11 13:37:44 +0200

    Add BoilerBuffers.en/decodeBufferedImageRGBA

--- src/main/java/com/github/knokko/boiler/buffer/BoilerBuffers.java
+++ src/main/java/com/github/knokko/boiler/buffer/BoilerBuffers.java
@@ -5,8 +5,12 @@ import org.lwjgl.util.vma.VmaAllocationCreateInfo;
 import org.lwjgl.util.vma.VmaAllocationInfo;
 import org.lwjgl.vulkan.VkBufferCreateInfo;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
 import static com.github.knokko.boiler.exceptions.VulkanFailureException.assertVmaSuccess;
 import static org.lwjgl.system.MemoryStack.stackPush;
+import static org.lwjgl.system.MemoryUtil.memByteBuffer;
 import static org.lwjgl.util.vma.Vma.*;
 import static org.lwjgl.util.vma.Vma.VMA_ALLOCATION_CREATE_MAPPED_BIT;
 import static org.lwjgl.vulkan.VK10.*;
@@ -67,5 +71,42 @@ public class BoilerBuffers {
             instance.debug.name(stack, pBuffer.get(0), VK_OBJECT_TYPE_BUFFER, name);
             return new MappedVmaBuffer(pBuffer.get(0), pAllocation.get(0), size, pInfo.pMappedData());
         }
+    }
+
+    public void encodeBufferedImageRGBA(MappedVmaBuffer mappedBuffer, BufferedImage image, long offset) {
+        int requiredSize = 4 * image.getWidth() * image.getHeight();
+        if (offset + requiredSize > mappedBuffer.size()) throw new IllegalArgumentException("Buffer is too small");
+
+        var byteBuffer = memByteBuffer(mappedBuffer.hostAddress() + offset, requiredSize);
+        for (int y = 0; y < image.getHeight(); y++) {
+            for (int x = 0; x < image.getWidth(); x++) {
+                var pixel = new Color(image.getRGB(x, y), true);
+                byteBuffer.put((byte) pixel.getRed());
+                byteBuffer.put((byte) pixel.getGreen());
+                byteBuffer.put((byte) pixel.getBlue());
+                byteBuffer.put((byte) pixel.getAlpha());
+            }
+        }
+    }
+
+    public BufferedImage decodeBufferedImageRGBA(MappedVmaBuffer mappedBuffer, long offset, int width, int height) {
+        int requiredSize = 4 * width * height;
+        if (offset + requiredSize > mappedBuffer.size()) throw new IllegalArgumentException("Buffer is too small");
+
+        var image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        var byteBuffer = memByteBuffer(mappedBuffer.hostAddress() + offset, requiredSize);
+        for (int y = 0; y < image.getHeight(); y++) {
+            for (int x = 0; x < image.getWidth(); x++) {
+                var pixel = new Color(
+                        byteBuffer.get() & 0xFF,
+                        byteBuffer.get() & 0xFf,
+                        byteBuffer.get() & 0xFF,
+                        byteBuffer.get() & 0xFF
+                );
+                image.setRGB(x, y, pixel.getRGB());
+            }
+        }
+
+        return image;
     }
 }
---
+++ src/test/java/com/github/knokko/boiler/buffer/TestImageEncode.java
@@ -0,0 +1,101 @@
+package com.github.knokko.boiler.buffer;
+
+import com.github.knokko.boiler.builder.BoilerBuilder;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.lwjgl.system.MemoryUtil.memGetByte;
+import static org.lwjgl.system.MemoryUtil.memPutByte;
+import static org.lwjgl.vulkan.VK10.VK_API_VERSION_1_0;
+import static org.lwjgl.vulkan.VK10.VK_BUFFER_USAGE_STORAGE_BUFFER_BIT;
+import static org.lwjgl.vulkan.VK11.VK_API_VERSION_1_1;
+import static org.lwjgl.vulkan.VK12.VK_API_VERSION_1_2;
+
+public class TestImageEncode {
+
+	@Test
+	public void testEncodeBufferedImageRGBA() {
+		var boiler = new BoilerBuilder(
+				VK_API_VERSION_1_0, "TestEncodeBufferedImageRGBA", 1
+		).validation().forbidValidationErrors().build();
+
+		var buffer = boiler.buffers.createMapped(10, VK_BUFFER_USAGE_STORAGE_BUFFER_BIT, "DestBuffer");
+		var image = new BufferedImage(1, 2, BufferedImage.TYPE_INT_ARGB);
+		image.setRGB(0, 0, Color.RED.getRGB());
+		image.setRGB(0, 1, new Color(50, 100, 150, 200).getRGB());
+
+		boiler.buffers.encodeBufferedImageRGBA(buffer, image, 1);
+		assertEquals((byte) 255, memGetByte(buffer.hostAddress() + 1));
+		assertEquals((byte) 0, memGetByte(buffer.hostAddress() + 2));
+		assertEquals((byte) 0, memGetByte(buffer.hostAddress() + 3));
+		assertEquals((byte) 255, memGetByte(buffer.hostAddress() + 4));
+
+		assertEquals((byte) 50, memGetByte(buffer.hostAddress() + 5));
+		assertEquals((byte) 100, memGetByte(buffer.hostAddress() + 6));
+		assertEquals((byte) 150, memGetByte(buffer.hostAddress() + 7));
+		assertEquals((byte) 200, memGetByte(buffer.hostAddress() + 8));
+
+		buffer.destroy(boiler.vmaAllocator());
+		boiler.destroyInitialObjects();
+	}
+
+	@Test
+	public void testDecodeBufferedImageRGBA() {
+		var boiler = new BoilerBuilder(
+				VK_API_VERSION_1_1, "TestDecodeBufferedImageRGBA", 1
+		).validation().forbidValidationErrors().build();
+
+		var buffer = boiler.buffers.createMapped(10, VK_BUFFER_USAGE_STORAGE_BUFFER_BIT, "DestBuffer");
+
+		memPutByte(buffer.hostAddress() + 1, (byte) 255);
+		memPutByte(buffer.hostAddress() + 2, (byte) 0);
+		memPutByte(buffer.hostAddress() + 3, (byte) 0);
+		memPutByte(buffer.hostAddress() + 4, (byte) 255);
+
+		memPutByte(buffer.hostAddress() + 5, (byte) 50);
+		memPutByte(buffer.hostAddress() + 6, (byte) 100);
+		memPutByte(buffer.hostAddress() + 7, (byte) 150);
+		memPutByte(buffer.hostAddress() + 8, (byte) 200);
+
+		var image = boiler.buffers.decodeBufferedImageRGBA(buffer, 1, 1, 2);
+		assertEquals(Color.RED, new Color(image.getRGB(0, 0), true));
+		assertEquals(new Color(50, 100, 150, 200), new Color(image.getRGB(0, 1), true));
+
+		buffer.destroy(boiler.vmaAllocator());
+		boiler.destroyInitialObjects();
+	}
+
+	@Test
+	public void testBufferedImageCodingWithoutAlpha() {
+		var boiler = new BoilerBuilder(
+				VK_API_VERSION_1_2, "TestBufferedImageCodingWithoutAlpha", 1
+		).validation().forbidValidationErrors().build();
+
+		var buffer = boiler.buffers.createMapped(10, VK_BUFFER_USAGE_STORAGE_BUFFER_BIT, "DestBuffer");
+		var image = new BufferedImage(1, 2, BufferedImage.TYPE_INT_RGB);
+		image.setRGB(0, 0, Color.RED.getRGB());
+		image.setRGB(0, 1, new Color(50, 100, 150).getRGB());
+
+		boiler.buffers.encodeBufferedImageRGBA(buffer, image, 1);
+		assertEquals((byte) 255, memGetByte(buffer.hostAddress() + 1));
+		assertEquals((byte) 0, memGetByte(buffer.hostAddress() + 2));
+		assertEquals((byte) 0, memGetByte(buffer.hostAddress() + 3));
+		assertEquals((byte) 255, memGetByte(buffer.hostAddress() + 4));
+
+		assertEquals((byte) 50, memGetByte(buffer.hostAddress() + 5));
+		assertEquals((byte) 100, memGetByte(buffer.hostAddress() + 6));
+		assertEquals((byte) 150, memGetByte(buffer.hostAddress() + 7));
+		assertEquals((byte) 255, memGetByte(buffer.hostAddress() + 8));
+
+		var copied = boiler.buffers.decodeBufferedImageRGBA(buffer, 1, image.getWidth(), image.getHeight());
+		assertEquals(image.getRGB(0, 0), copied.getRGB(0, 0));
+		assertEquals(image.getRGB(0, 1), copied.getRGB(0, 1));
+		assertEquals(255, new Color(copied.getRGB(0, 0), true).getAlpha());
+
+		buffer.destroy(boiler.vmaAllocator());
+		boiler.destroyInitialObjects();
+	}
+}
