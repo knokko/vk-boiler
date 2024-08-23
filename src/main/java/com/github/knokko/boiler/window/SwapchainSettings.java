d970b10ecc60ee6d8606d144cfea6d5b30ebda1b
knokko <knokogator@hotmail.com>
2024-08-23 20:42:51 +0200

    Delete the unused WindowSurface and SwapchainSettings records

--- src/main/java/com/github/knokko/boiler/window/SwapchainSettings.java
+++
@@ -1,6 +0,0 @@
-package com.github.knokko.boiler.window;
-
-import com.github.knokko.boiler.builders.window.SurfaceFormat;
-
-public record SwapchainSettings(int imageUsage, SurfaceFormat surfaceFormat, int compositeAlpha) {
-}
