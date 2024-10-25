8ed1f4e15a6b79dd03e14413647869643303daae
knokko <knokogator@hotmail.com>
2024-10-25 13:27:23 +0200

    Add nextMultipleOf overload for int

--- src/main/java/com/github/knokko/boiler/utilities/BoilerMath.java
+++ src/main/java/com/github/knokko/boiler/utilities/BoilerMath.java
@@ -11,6 +11,20 @@ public class BoilerMath {
 		long quotient = value / alignment;
 		long reverted = quotient * alignment;
 		if (reverted < value) reverted += alignment;
+		if (reverted < 0L) throw new IllegalArgumentException("Long overflow: " + value + " and " + alignment);
+		return reverted;
+	}
+
+	/**
+	 * @return The smallest integer multiple of <i>alignment</i> that is greater than or equal to <i>value</i>
+	 * @throws IllegalArgumentException If either of the parameters is negative
+	 */
+	public static int nextMultipleOf(int value, int alignment) {
+		if (value < 0 || alignment < 0) throw new IllegalArgumentException("Both parameters must be non-negative");
+		int quotient = value / alignment;
+		int reverted = quotient * alignment;
+		if (reverted < value) reverted += alignment;
+		if (reverted < 0) throw new IllegalArgumentException("Integer overflow: " + value + " and " + alignment);
 		return reverted;
 	}
 }
--- src/test/java/com/github/knokko/boiler/utilities/TestBoilerMath.java
+++ src/test/java/com/github/knokko/boiler/utilities/TestBoilerMath.java
@@ -8,12 +8,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TestBoilerMath {
 
 	@Test
-	public void testNextMultipleOf() {
+	public void testNextMultipleOfInt() {
 		assertEquals(10, nextMultipleOf(10, 1));
 		assertEquals(10, nextMultipleOf(10, 2));
 		assertEquals(10, nextMultipleOf(10, 5));
 		assertEquals(10, nextMultipleOf(10, 10));
 		assertEquals(12, nextMultipleOf(10, 3));
 		assertEquals(12, nextMultipleOf(10, 4));
+	}
+
+	@Test
+	public void testNextMultipleOfLong() {
+		assertEquals(10L, nextMultipleOf(10L, 1L));
+		assertEquals(10L, nextMultipleOf(10L, 2L));
+		assertEquals(10L, nextMultipleOf(10, 5L));
+		assertEquals(10L, nextMultipleOf(10, 10L));
+		assertEquals(12L, nextMultipleOf(10L, 3));
+		assertEquals(12L, nextMultipleOf(10L, 4));
 	}
 }
