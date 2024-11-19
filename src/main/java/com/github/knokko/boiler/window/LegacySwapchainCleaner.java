59181cc6c9b5f5129da62579aa304886ba25c183
knokko <knokogator@hotmail.com>
2024-11-19 10:58:42 +0100

    Fix potential bug in LegacySwapchainCleaner

--- src/main/java/com/github/knokko/boiler/window/LegacySwapchainCleaner.java
+++ src/main/java/com/github/knokko/boiler/window/LegacySwapchainCleaner.java
@@ -11,22 +11,22 @@ class LegacySwapchainCleaner extends SwapchainCleaner {
 	@Override
 	boolean chooseRemainingImages(State state, List<AcquiredImage> remainingImages) {
 		var canDestroyOlderImages = new boolean[state.swapchain().images.length];
-		boolean canDestroyOldSwapchains = false;
+		boolean hasDestroyedOldImage = false;
 
 		for (int myImageIndex = state.acquiredImages().size() - 1; myImageIndex >= 0; myImageIndex--) {
 			var image = state.acquiredImages().get(myImageIndex);
 			if (canDestroyOlderImages[image.index()]) {
 				destroyImageNow(image, true);
+				hasDestroyedOldImage = true;
 			} else {
 				remainingImages.add(image);
 				if (image.acquireFence.isSignaled()) {
-					canDestroyOldSwapchains = true;
 					canDestroyOlderImages[image.index()] = true;
 				}
 			}
 		}
 
-		return canDestroyOldSwapchains;
+		return hasDestroyedOldImage;
 	}
 
 	@Override
